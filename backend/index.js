d289669caa5466d0c5ee13bcdc7cd87afa4c855e
abubakarSaddiquee <abubakar.saddiq303@gmail.com>
2022-03-22 16:27:17 +0500

    mongodb connectoin updated

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const Port = process.env.PORT || 5000;
+const Note = require("./model/userSchema");
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -12,12 +13,22 @@ const getuser = require("./router/getUser");
 const postuser = require("./router/postUser");
 const deleteuser = require("./router/DeleteUser");
 
-app.use("/getuser", getuser);
+// app.use("/getuser", getuser);
 app.use("/postuser", postuser);
 app.use("/deleteuser", deleteuser);
 
 app.get("/", (req, res) => {
   res.send("My Home page");
+});
+app.get("/getuser", async (req, res) => {
+  try {
+    // const { title, text, color } = req.body;
+    const getData = await Note.find();
+    res.status(200).json({ message: "Showed notes", getData });
+  } catch {
+    (err) => console.log(err);
+    res.send(404);
+  }
 });
 
 app.listen(Port, () => {
