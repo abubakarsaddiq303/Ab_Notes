b612e9968d183e0f6c1958eb6f1617ad6517aa8d
abubakarSaddiquee <abubakar.saddiq303@gmail.com>
2022-03-22 16:03:30 +0500

    mongodb connectoin updated

--- backend/router/getUser.js
+++ backend/router/getUser.js
@@ -4,11 +4,12 @@ const Note = require("../model/userSchema");
 
 router.get("/", async (req, res) => {
   try {
-    const { title, text, color } = req.body;
-    const getData = await Note.find();
-    res.status(200).json({ message: "Showed notes", getData });
+    // const { title, text, color } = req.body;
+    // const getData = await Note.find();
+    res.status(200).json({ message: "Showed notes" });
   } catch {
     (err) => console.log(err);
+    res.send(404);
   }
 });
 
